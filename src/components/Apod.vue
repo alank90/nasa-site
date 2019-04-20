a7734630b2f8c8e5c627e63af05a92903cc818b1
Alan K <akillian@outlook.com>
2019-04-19 21:04:14 -0400

    commit

--- src/components/Apod.vue
+++ src/components/Apod.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="apod">
-    <b-img :src="imgSrc" fluid alt="Planet logo"></b-img>
+    <div v-if="loading">
+      Loading...
+      <!--Loading spinner -->
+      <i class="fas fa-spinner spin-it" aria-hidden="true"></i>
+    </div>
+
+    <b-jumbotron v-bind:header="title">
+      <b-img :src="imgSrc" fluid alt=""></b-img>
+      <p>{{ explanation }}</p>
+    </b-jumbotron>
   </div>
 </template>
 
@@ -8,13 +17,14 @@
 import axios from "axios";
 import api from "../nasaKey.js";
 
-console.log(api.key);
-
 export default {
   name: "apod",
   data() {
     return {
-      imgSrc: require("../assets/Neptune.jpg")
+      imgSrc: "",
+      title: "",
+      explanation: "",
+      loading: true
     };
   },
   mounted() {
@@ -23,9 +33,14 @@ export default {
   methods: {
     getApod() {
       const url = `https://api.nasa.gov/planetary/apod?api_key=${api.key}`;
-      console.log("url is: " + url);
+
       axios.get(url).then(response => {
-        console.log(response);
+        const data = response.data;
+        console.log(data);
+        this.title = data.title;
+        this.explanation = data.explanation;
+        this.imgSrc = data.url;
+        this.loading = false;
       });
     }
   }
@@ -33,8 +48,20 @@ export default {
 </script>
 
 <style>
+h1.display-3 {
+  font-size: 2rem;
+}
+
+p {
+  margin-top: 30px;
+}
+
 img.img-fluid {
   max-width: 800px;
+}
+
+.jumbotron {
+  background-color: aliceblue;
 }
 </style>
 
