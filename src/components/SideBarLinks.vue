886169b0dd24a98cdb771d8a76974d89c15bc1f6
Alan K <akillian@outlook.com>
2019-04-24 13:59:10 -0400

    commit

--- src/components/SideBarLinks.vue
+++ src/components/SideBarLinks.vue
@@ -16,7 +16,22 @@
       <b-list-group-item v-on:click="sendDataApod()" href="#">Astronomy Picture of the Day</b-list-group-item>
       <b-list-group-item v-b-toggle="'collapse-2'" class="m-1">NASA Images Library</b-list-group-item>
       <b-collapse id="collapse-2">
-        <b-list-group-item v-on:click="getNasaImages()" href="#">Search</b-list-group-item>
+        <b-list-group-item href="#">
+          <b-form v-if="show">
+            <b-form-group id="input-group-1" label="Search NASA Library" label-for="input-1">
+              <b-form-input
+                id="input-1"
+                v-model="form.searchTerm"
+                type="text"
+                required
+                placeholder="Enter Query"
+              ></b-form-input>
+            </b-form-group>
+
+            <b-button v-on:click="onSubmit" variant="primary">Submit</b-button>
+            <b-button v-on:click="onReset" variant="danger">Reset</b-button>
+          </b-form>
+        </b-list-group-item>
       </b-collapse>
       <b-list-group-item href="#">Action links are easy</b-list-group-item>
       <b-list-group-item href="#foobar" disabled>Disabled link</b-list-group-item>
@@ -29,7 +44,11 @@ export default {
   name: "SideBarLinks",
   data() {
     return {
-      displayGroupItem: false
+      displayGroupItem: false,
+      form: {
+        searchTerm: ""
+      },
+      show: true
     };
   },
   methods: {
@@ -45,8 +64,19 @@ export default {
         this.displayGroupItem = true;
       }
     },
-    getNasaImages() {
-      console.log("Clicked to see NASA Pictures.");
+    onSubmit(evt) {
+      evt.preventDefault();
+      console.log(this.form.searchTerm);
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      // reset for values
+      this.form.searchTerm = "";
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
     }
   }
 };
@@ -54,8 +84,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.list-group-item-action:hover {
+.list-group-item-action:hover,
+.list-group-item:hover {
   background-color: #0065b34b;
+  cursor: pointer;
 }
 .img-fluid {
   max-width: 60%;
