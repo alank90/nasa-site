f488525668afbdca26ab336818738d868a70ece9
Alan K <akillian@outlook.com>
2019-05-07 14:22:12 -0400

    commit add vueidate...

--- src/components/SideBarLinks.vue
+++ src/components/SideBarLinks.vue
@@ -21,11 +21,26 @@
       <b-list-group-item v-b-toggle="'collapse-2'" class="m-1">NASA Images Library</b-list-group-item>
       <b-collapse id="collapse-2">
         <b-list-group-item href="#">
+          <!--=============== Vuelidate Markup ========================== -->
+          <div v-if="$v.formResponses.q.$error">
+            <p class="error" v-if="!$v.formResponses.q.required">Name is required</p>
+            <p
+              class="error"
+              v-if="!$v.formResponses.q.minLength"
+            >Name must have at least {{$v.formResponses.q.$params.minLength.min}} letters.</p>
+          </div>
+
+          <p class="typo__p" v-if="submitStatus === 'OK'">Thanks for your submission!</p>
+          <p class="typo__p" v-if="submitStatus === 'ERROR'">Please fill the form correctly.</p>
+          <p class="typo__p" v-if="submitStatus === 'PENDING'">Sending...</p>
+          <!-- ================ End Vuelidate  Markup ============== -->
+
           <b-form v-if="show">
-            <b-form-group id="input-group-1" label="Enter Query" label-for="input-1">
+            <b-form-group id="input-group-1" label="* Enter Query" label-for="input-1">
               <b-form-input
+                class="full form__input"
                 id="input-1"
-                v-model="form.q"
+                v-model.lazy="$v.formResponses.q.$model"
                 type="text"
                 required
                 placeholder="e.g. apollo 11 landing"
@@ -35,7 +50,7 @@
             <b-form-group id="input-group-2" label="Search Media Type" label-for="input-2">
               <b-form-input
                 id="input-2"
-                v-model="form.media_type"
+                v-model="formResponses.media_type"
                 type="text"
                 placeholder="Enter media-type e.g. image"
               ></b-form-input>
@@ -44,17 +59,26 @@
             <b-form-group id="input-group-3" label="Search Start Year" label-for="input-3">
               <b-form-input
                 id="input-3"
-                v-model="form.year_start"
+                v-model="formResponses.year_start"
                 type="text"
                 placeholder="optional"
               ></b-form-input>
             </b-form-group>
 
             <b-form-group id="input-group-4" label="Search End Year" label-for="input-4">
-              <b-form-input id="input-4" v-model="form.year_end" type="text" placeholder="optional"></b-form-input>
+              <b-form-input
+                id="input-4"
+                v-model="formResponses.year_end"
+                type="text"
+                placeholder="optional"
+              ></b-form-input>
             </b-form-group>
 
-            <b-button v-on:click="onSubmit" variant="primary">Submit</b-button>
+            <b-button
+              v-on:click="onSubmit"
+              variant="primary"
+              :disabled="submitStatus === 'PENDING'"
+            >Submit</b-button>
             <b-button v-on:click="onReset" variant="danger">Reset</b-button>
           </b-form>
         </b-list-group-item>
@@ -74,18 +98,19 @@ export default {
     return {
       displayBackToFront: false,
       displayApod: true,
-      form: {
+      formResponses: {
         q: "",
         media_type: "",
         year_start: "",
         year_end: ""
       },
+      submitStatus: null,
       show: true
     };
   },
   validations: {
     formResponses: {
-      name: {
+      q: {
         required,
         minLength: minLength(2)
       }
@@ -108,46 +133,56 @@ export default {
     },
     onSubmit(evt) {
       evt.preventDefault();
-      let formValues = this.form;
-      let uri = `https://images-api.nasa.gov/search?`;
-      // This is .reduce method for objects. If key value is empty
-      // string delete it.
-      Object.entries(formValues).forEach(([key, value]) => {
-        if (value === "") {
-          delete formValues[key];
-        } else {
-          uri += `${key}=${formValues[key]}&`; // Build the url request
-        }
-      });
-      uri = uri.substring(0, uri.length - 1); // Delete trailing &
-      const url = encodeURI(uri); // Encode url request for http transport to server
 
-      // Send off API request to NASA
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          const results = data.collection;
-          console.log(results);
-
-          // Change View to SearchResults
-          this.$eventBus.$emit("send-data", "SearchResults");
-          if (this.displayBackToFront === false) {
-            this.displayBackToFront = true;
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        this.submitStatus = "ERROR";
+      } else {
+        let uri = `https://images-api.nasa.gov/search?`;
+        // This is .reduce method for objects. If key value is empty
+        // string delete it.
+        Object.entries(this.formResponses).forEach(([key, value]) => {
+          if (value === "") {
+            delete this.formResponses[key];
+          } else {
+            uri += `${key}=${this.formResponses[key]}&`; // Build the url request
           }
-
-          // Send NASA Search Data results on event Bus to Home.vue
-          this.$eventBus.$emit("nasa-data", results);
         });
+        uri = uri.substring(0, uri.length - 1); // Delete trailing &
+        const url = encodeURI(uri); // Encode url request for http transport to server
+
+        // Send off API request to NASA
+        fetch(url)
+          .then(response => response.json())
+          .then(data => {
+            const results = data.collection;
+            console.log(results);
+
+            // Change View to SearchResults
+            this.$eventBus.$emit("send-data", "SearchResults");
+            if (this.displayBackToFront === false) {
+              this.displayBackToFront = true;
+            }
+
+            // Send NASA Search Data results on event Bus to Home.vue
+            this.$eventBus.$emit("nasa-data", results);
+          });
+
+        this.submitStatus = "PENDING";
+        setTimeout(() => {
+          this.submitStatus = "OK";
+        }, 500);
+      }
     },
     onReset(evt) {
       evt.preventDefault();
 
-      // reset form values
-      this.form.q = "";
-      this.form.media_type = "";
-      this.form.year_start = "";
-      this.form.year_end = "";
-      // Trick to reset/clear native browser form validation state
+      // reset formResponses values
+      this.formResponses.q = "";
+      this.formResponses.media_type = "";
+      this.formResponses.year_start = "";
+      this.formResponses.year_end = "";
+      // Trick to reset/clear native browser formResponses validation state
       this.show = false;
       this.$nextTick(() => {
         this.show = true;
@@ -170,5 +205,9 @@ export default {
 
 .btn-primary {
   margin-right: 5px;
+}
+
+.error {
+  color: red;
 }
 </style>
